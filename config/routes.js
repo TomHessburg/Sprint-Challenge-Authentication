1ac9dbbef766a6abc14fd3d43d25e25a8b7a9623
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-04-05 11:51:24 -0400

    MVP complete, sorry for not doing a commit earlier but this project was super short

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
-
 const { authenticate } = require('../auth/authenticate');
+const db = require('../database/dbConfig.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,35 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body
+    user.password = bcrypt.hashSync(user.password, 8);
+    console.log(user);
+
+    db('users')
+        .insert(user)
+        .then(response => res.status(200).json(response))
+        .catch(err => res.status(500).json({errMessage: "there as an error retrieving your resources, either you havent completed the form or that username is already taken..."}))
+
 }
 
 function login(req, res) {
-  // implement user login
+
+  const { username, password } = req.body
+    db('users')
+        .where({ username })
+        .first()
+        .then(user => {
+            if(bcrypt.compareSync(password, user.password)){
+                const token = generateToken(user)
+                res.status(200).json({
+                    message: "welcome back!",
+                    token
+                });
+            }else{
+                res.status(401).json({message: "invalid credentials"})
+            }
+        })
+        .catch(err => res.status(500).json({errMessage: "unable to log you in!"}))
 }
 
 function getJokes(req, res) {
@@ -29,4 +57,17 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+
+function generateToken(user){
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '4h'
+  };
+  
+  return jwt.sign(payload, secret, options)
 }
